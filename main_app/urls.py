684a9f82ce8ab25f8d0deded295a8d41395d5de7
Brent <brentmueller32@gmail.com>
2023-08-23 08:26:22 -0700

    sign up

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
   path('meals/', views.meals_index, name='index'),
   path('meals/<int:meal_id>/', views.meals_detail, name='detail'),
   path('meals/create/', views.MealCreate.as_view(), name='meals_create'),
-  #path('',),
-   path('accounts/signup/', views.signup, name='signup'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
